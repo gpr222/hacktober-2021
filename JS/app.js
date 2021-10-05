f8ab82245eef79ae32f280fa4c40dca62bcb3faf
staykirak <18211a05h5@bvrit.ac.in>
2021-10-06 00:14:50 +0530

    Create dashboard

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+function test() {
+	var pass = document.getElementById("pass");
+	var hideshow = document.getElementById("hideshow");
+	if (pass.type == "text") {
+		pass.type = "password";
+		hideshow.innerHTML = "show";
+	} else {
+		pass.type = "text";
+		hideshow.innerHTML = "hide";
+	}
+}
